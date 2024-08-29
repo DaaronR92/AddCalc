18102940e8b2944de816cffbb21a43ed4bda73f4
David <85143249+DaaronR92@users.noreply.github.com>
2024-08-28 22:23:33 -0400

    Add files via upload

---
+++ addCalc.py
@@ -0,0 +1,15 @@
+# A simple addition calculator
+
+# This is used to input you numbers
+number1 = input("Enter the first number ")
+number2 = input("Enter the second number ")
+
+# This converts the inputs to floats
+number1 = float(number1)
+number2 = float(number2)
+
+# This performs the addition
+result = number1 + number2
+
+# This displays the result
+print(f"The result of {number1} and {number2} is: {result}")
